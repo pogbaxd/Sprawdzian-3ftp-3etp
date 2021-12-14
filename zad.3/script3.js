2f68d345afa75f7bd3e1fea6dbcace01b3bc5456
Mikołaj Miszka <mikolaj150878@gmail.com>
2021-12-14 17:52:20 +0100

    zad.3.2

--- zad.3/script3.js
+++ zad.3/script3.js
@@ -1,11 +1,11 @@
-let firstNumber = Number(prompt('Wpisz pierwszą liczbe'));
+let firstNumber = Number(prompt('Wpisz pierwszą liczbe'));// zapytanie o pierwsza liczbę
 
-function between(min, max) {
+function between(min, max) { // funkacja losująca drugą liczbę z podanego przedziału
     return Math.floor(
         Math.random() * (max - min) + min
     )
 }
 
-let secondNumber = Number(between(0,50));
+let secondNumber = Number(between(0,50)); // wylosowanie drugiej liczby  z przedziału od 0 do 50
 
-document.write("iloczyn to: " + secondNumber*firstNumber);
+document.write("iloczyn to: " + secondNumber*firstNumber);// wypisanie na dokument wyniku
