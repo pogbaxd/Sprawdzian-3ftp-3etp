dcb58e7f30e459db2f4bd2c3fd2bb63e6e44a06a
Mikołaj Miszka <mikolaj150878@gmail.com>
2021-12-15 18:44:39 +0100

    zad.5.2

--- zad.5/script5.js
+++ zad.5/script5.js
@@ -1,5 +1,7 @@
-let firstNumber = Number(prompt('wpisz pierwszą liczbę: '));
+let firstNumber = Number(prompt('wpisz pierwszą liczbę: '));// zapytania o liczby
 let secondNumber = Number(prompt('wpisz drugą liczbę: '));
 let thirdNumber = Number(prompt('wpisz trzecisz liczbę: '));
 
-document.write(`Największa liczba to : ${Math.max(firstNumber, secondNumber, thirdNumber)}`);
+let result = Math.max(firstNumber, secondNumber, thirdNumber);// znalezienie największej liczby
+
+document.write(`Największa liczba to : ${result}`);
