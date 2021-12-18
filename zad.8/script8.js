9ee5271bae66fe179c1bb551d05520add62e32c4
Mikołaj Miszka <mikolaj150878@gmail.com>
2021-12-18 16:36:31 +0100

    zad.7.2

---
+++ zad.8/script8.js
@@ -0,0 +1,11 @@
+let n = Number(prompt('wpisz długość ciągu ciągu'));
+let x = Number(prompt('wpisz liczbę początkową'));
+let y = Number(prompt('wpisz różnice między liczbami'));
+
+document.write("ciąg arytmetyczny : ");
+document.write(`${x} \n`);
+for (let i=0; i<n-1; i++) {
+
+    x += y;
+    document.write(`${x} \n`);
+}
